393a5b18a0656e31e0fe878edc2bf0ec3ed43ae5
Tomer <tomereli@m.telhai.ac.il>
2026-09-06 15:54:33 +0300

    add logger setup functionality

---
+++ logger.py
@@ -0,0 +1,25 @@
+import logging
+
+"""
+usage
+
+from logger import setup_logger
+import logging
+
+setup_logger("CLIENT") / setup_logger("SERVER")
+
+logger = logging.getLogger(__name__)
+
+logger.info("User %s joined room %s", username, room)
+logger.debug("debug: User %s joined room %s", username, room)
+"""      
+
+def setup_logger(role: str):
+    logging.basicConfig(
+        level=logging.INFO,
+        format=f"%(asctime)s | {role} | %(levelname)s | %(name)s | %(message)s",
+        handlers=[
+            logging.StreamHandler(),          # console
+            logging.FileHandler(f"app_{role.lower()}.log")    # file
+        ]
+    )
